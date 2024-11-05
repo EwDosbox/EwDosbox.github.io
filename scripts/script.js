a148e48347eee0c5edcf2a26e3376e52f0ce0f81
EwDosbox <144440258+EwDosbox@users.noreply.github.com>
2024-11-05 21:16:47 +0100

    updated extensions and CSS with JS

--- scripts/script.js
+++ scripts/script.js
@@ -7,17 +7,17 @@ function OnStart() {
 function BackgroundSlideshow() {
     let element = document.querySelector('#hero-container-slideshow');
     const backgrounds = [
-        'https://ewdosbox.github.io/images/fraid1.jpg',
-        'https://ewdosbox.github.io/images/fraid2.jpg',
-        'https://ewdosbox.github.io/images/PlatBorne1.jpg',
-        'https://ewdosbox.github.io/images/PlatBorne2.jpg',
-        'https://ewdosbox.github.io/images/PlatBorne3.jpg'
+        'https://ewdosbox.github.io/images/fraid1.png',
+        'https://ewdosbox.github.io/images/fraid2.png',
+        'https://ewdosbox.github.io/images/PlatBorne1.png',
+        'https://ewdosbox.github.io/images/PlatBorne2.png',
+        'https://ewdosbox.github.io/images/PlatBorne3.png'
     ];
 
     let currentIndex = 0;
 
     function changeBackground() {
-        element.style.backgroundImage = backgrounds[currentIndex];
+        element.style.backgroundImage = `url('${backgrounds[currentIndex]}')`;
         currentIndex = (currentIndex + 1) % backgrounds.length; // Loop back to the first image
     }
 
